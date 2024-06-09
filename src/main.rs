c4abcf309f3706aa5a27f6d5d2207e6c85e491ff
nbrr <nbrr@protonmail.com>
2024-06-09 15:35:22 +0200

    Add insert.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use diesel::prelude::*;
 use rocket::serde::json::Json;
 use rocket_sync_db_pools::database;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 #[macro_use]
@@ -12,7 +12,7 @@ mod schema;
 #[database("itk_db")]
 struct DbConnection(diesel::PgConnection);
 
-#[derive(Identifiable, Queryable, Selectable, Serialize)]
+#[derive(Identifiable, Queryable, Selectable, Insertable, Serialize, Deserialize)]
 #[diesel(table_name=schema::my_elements)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct MyElement {
@@ -33,9 +33,21 @@ async fn my_route(conn: DbConnection) -> Json<Vec<MyElement>> {
     .await
 }
 
+#[post("/", format = "application/json", data = "<my_new_element>")]
+async fn my_post_route(conn: DbConnection, my_new_element: Json<MyElement>) -> Json<MyElement> {
+    conn.run(move |c| {
+        let my_element = diesel::insert_into(schema::my_elements::table)
+            .values(&my_new_element.0)
+            .get_result(c)
+            .unwrap();
+
+        Json(my_element)
+    })
+    .await
+}
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .attach(DbConnection::fairing())
-        .mount("/", routes![my_route])
+        .mount("/", routes![my_route, my_post_route])
 }
